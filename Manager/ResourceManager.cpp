42e6f40b612bf4aa3788fedd82434778c33db13b
HW <ekdrn1@naver.com>
2026-06-05 21:36:58 +0900

    ResourceManager 구현

---
+++ Manager/ResourceManager.cpp
@@ -0,0 +1,21 @@
+#include "Engine/pch.h"
+#include "ResourceManager.h"
+
+namespace engine
+{
+	ResourceManager::ResourceManager()
+	{}
+	ResourceManager::~ResourceManager()
+	{}
+	bool ResourceManager::AddResource(const stdfs::path& path, s_ptr<Resource> resource)
+	{
+		auto iter = resources_.find(path);
+		if (iter != resources_.end())
+		{
+			return false;
+		}
+
+		resources_[path] = resource;
+		return true;
+	}
+}
---
+++ Manager/ResourceManager.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <Engine/Common.h>
+#include <Engine/Singleton.h>
+
+namespace engine
+{
+	class Resource;
+	class ResourceManager
+	{
+		DECLARE_SINGLETON(ResourceManager)
+
+	public:
+		template <typename T>
+		s_ptr<T> Find(const stdfs::path& path)
+		{
+			auto iter = resources_.find(path);
+			if (iter != resources_.end())
+			{
+				return std::dynamic_pointer_cast<T>(iter->second);
+			}
+			return nullptr;
+		}
+
+		template <typename T>
+		s_ptr<T> LoadFromFile(const stdfs::path& path)
+		{
+			s_ptr<T> resource = Find<T>(path);
+			if (resource) { return resource; }
+
+			resource = std::make_shared<T>();
+			if (false == resource->LoadFromFile(path))
+			{ 
+				return nullptr; 
+			}
+				
+			resources_[path] = resource;
+			return resource;
+		}
+
+		bool AddResource(const stdfs::path& path, s_ptr<Resource> resource);
+
+
+	private:
+		std::unordered_map <stdfs::path, s_ptr<Resource>> resources_;
+	};
+}
+
+

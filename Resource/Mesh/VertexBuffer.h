6031ba1757b8908ecd31d88786aa2a90e33b6b27
HW <ekdrn1@naver.com>
2026-06-08 10:36:02 +0900

    unsigned char -> uint8

--- Resource/Mesh/VertexBuffer.h
+++ Resource/Mesh/VertexBuffer.h
@@ -26,8 +26,8 @@ namespace engine
         void Bind(const ComPtr<ID3D11DeviceContext>& context);
 
 	private:
-        //Vertex 구조체는 언제든지 달라질수 있음 -> 통합 저장을 위해 unsigned char(byte) 형태로 저장.
-        std::vector<unsigned char> data_ = {};
+        //Vertex 구조체는 언제든지 달라질수 있음 -> 통합 저장을 위해 uint8(byte) 형태로 저장.
+        std::vector<uint8> data_ = {};
         UINT data_stride_ = {};
         UINT data_count_ = {};
 

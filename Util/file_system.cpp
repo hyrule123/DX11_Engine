04cf483fbeb803260db412ea3584449ebb3a1ac2
HW <ekdrn1@naver.com>
2026-08-16 15:27:20 +0900

    ReadAll 함수 추가

--- Util/file_system.cpp
+++ Util/file_system.cpp
@@ -11,19 +11,37 @@ namespace engine::file_system
 	std::vector<uint8> ReadAllBytes(const stdfs::path& file_path)
 	{
         std::ifstream file(file_path, std::ios::binary | std::ios::ate);
-
-        std::vector<uint8_t> buffer;
+        
         if (false == file.is_open()) {
             ERROR_MESSAGE("파일 열기 실패!!");
-            return buffer;
+            return {};
         }
 
-        std::streamsize size = file.tellg();
-        file.seekg(0, std::ios::beg);
+		return ReadAllBytes(file);
+	}
+    std::vector<uint8> ReadAllBytes(std::ifstream& ifs)
+    {
+        std::vector<uint8> buffer;
+
+        std::streamsize size = ifs.tellg();
+        ifs.seekg(0, std::ios::beg);
 
         buffer.resize(size);
-        file.read(reinterpret_cast<char*>(buffer.data()), size);
+        ifs.read(reinterpret_cast<char*>(buffer.data()), size);
 
         return buffer;
-	}
+    }
+    bool DumpFile(const stdfs::path& file_path, const std::vector<uint8>& data)
+    {
+		std::ofstream of(file_path, std::ios::binary);
+        
+        if (false == of.is_open())
+        {
+			ERROR_MESSAGE("파일 열기 실패!!");
+			return false;
+        }
+
+        of.write(reinterpret_cast<const char*>(data.data()), data.size());
+        return true;
+    }
 }
--- Util/file_system.h
+++ Util/file_system.h
@@ -2,10 +2,34 @@
 
 #include <Engine/Core/CoreMinimal.h>
 
+#include <filesystem>
+#include <fstream>
+#include <vector>
+
 namespace engine
 {
 	namespace file_system
 	{
 		std::vector<uint8> ReadAllBytes(const stdfs::path& file_path);
+		std::vector<uint8> ReadAllBytes(std::ifstream& ifs);
+
+		template <typename T>
+		std::vector<T> ReadAll(std::ifstream& fs)
+		{
+			fs.seekg(0, std::ios::end);
+			const std::streamoff bytes = fs.tellg();
+			fs.seekg(0, std::ios::beg);
+
+			if (bytes <= 0 || bytes % sizeof(T) != 0)
+			{
+				return {};
+			}
+
+			std::vector<T> out((size_t)bytes / sizeof(T));
+			fs.read((char*)out.data(), bytes);
+			return out;
+		}
+
+		bool DumpFile(const stdfs::path& file_path, const std::vector<uint8>& data);
 	}
 }
